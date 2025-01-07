155596d2f0d985b415d9486a82c2f12178f01666
Vijay Varma <vijay.varma@iongroup.com>
2025-01-07 12:37:55 +0530

    Added exception handling in common function to write json

--- data-scraping/includes/common_functions.py
+++ data-scraping/includes/common_functions.py
@@ -14,10 +14,14 @@ def initialize_selenium_driver():
 
 # Common function used to write data to json file with file_name
 def write_slot_to_json(file_name, json_data):
-    with open(file_name, 'w') as f:
-        json_data = json.dump(json_data, f, indent=4)
-        print(f"Data written to {file_name}")
-
+    try:
+        with open(file_name, 'w') as f:
+            json_data = json.dump(json_data, f, indent=4)
+            print(f"Data written to {file_name}")
+    except Exception as e:
+        print(f"Error writing JSON file: {e}")
+        
+        
 # Common function used to read data from json file with file_name
 def read_json_file(file_path, file_name):
     try:
