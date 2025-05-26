2f13cef62bab8572c999acc881d78b94fd401a61
Vijay Varma <vijay.varma@iongroup.com>
2025-05-26 12:43:01 +0530

    fix: add missing imports for Selenium driver in scrape_venue_address.py

--- src/data_injestion/scrape_venue_adress.py
+++ src/data_injestion/scrape_venue_adress.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
